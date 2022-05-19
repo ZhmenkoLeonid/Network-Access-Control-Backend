28d60d522fcf29132fa1ea5c686197aca9035cb6
Leonid Zhmenko <44261808+ZhmenkoLeonid@users.noreply.github.com>
2022-05-19 09:00:19 +0600

    init

--- host-validation/src/main/java/com/zhmenko/hostvalidation/HostValidation.java
+++
@@ -1,7 +0,0 @@
-package com.zhmenko.hostvalidation;
-
-import com.zhmenko.hostvalidation.host.ValidationPacket;
-
-public interface HostValidation {
-    public boolean check(ValidationPacket data);
-}
--- host-validation/src/main/java/com/zhmenko/hostvalidation/host/HostData.java
+++
@@ -1,7 +0,0 @@
-package com.zhmenko.hostvalidation.host;
-
-public interface HostData {
-    public String getIpAddress();
-    public String getMacAddress();
-    public String getHostname();
-}
