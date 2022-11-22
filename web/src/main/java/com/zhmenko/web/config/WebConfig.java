5c79834a5e426a9552a59148ed6d4eaf2606c9b1
Leonid Zhmenko <44261808+ZhmenkoLeonid@users.noreply.github.com>
2022-11-22 16:00:20 +0600

    add configuration

--- web/src/main/java/com/zhmenko/web/config/WebConfig.java
+++ web/src/main/java/com/zhmenko/web/config/WebConfig.java
@@ -6,10 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
-
 }
